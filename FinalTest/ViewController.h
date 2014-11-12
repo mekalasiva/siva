208374695bb6e9e392a143a3ea6baffc0f5bbe16
computer avenue <computeravenue@computers-MacBook-Pro.local>
2014-11-12 16:02:26 +0530

    Initial Commit

---
+++ FinalTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FinalTest
+//
+//  Created by computer avenue on 11/12/14.
+//  Copyright (c) 2014 CSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FinalTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FinalTest
+//
+//  Created by computer avenue on 11/12/14.
+//  Copyright (c) 2014 CSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
